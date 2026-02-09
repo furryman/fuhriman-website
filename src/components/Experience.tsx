65c27d74a79c9e09fae360b286eb6a120d51c902
Adam Fuhriman <adam@furryman.net>
2026-02-09 14:44:27 -0700

    updated experience

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -6,12 +6,13 @@ const experiences = [
     company: 'Mastercontrol',
     period: '2021 - 2025',
     highlights: [
-      'Architected and deployed multi-region Kubernetes clusters on AWS EKS for over 150 microservices',
-      'Implemented Atlantis, reducing Terraform wait times from minutes to seconds, enhancing productivity and accelerating development cycles',
-      'Implemented GitOps workflows with ArgoCD, reducing deployment time by 70%',
-      'Built comprehensive monitoring stack with Prometheus and Grafana',
-      'Built PoC for Internal Developer Portal (IDP) using Backstage',
-      'Led numerous optimization/refactoring projects for Terraform modules, cutting repeated code by over 50% and speeding up deployments'
+      'Managed and deployed multi-region Kubernetes clusters on AWS EKS for over 150 microservices',
+      'Implemented Atlantis for Terraform automation, reducing Terraform wait times from minutes to seconds, enhancing productivity, and accelerating development cycles',
+      'Coordinated GitOps workflows with ArgoCD and Helm, reducing deployment time by 70%, facilitating streamlined deployment, scaling, and management within dynamic containerized environments.',
+      'Managed and maintained CI/CD pipelines and version control systems in Github Actions, which cut lead times from a monthly cadence to weekly releases',
+      'Managed a comprehensive monitoring stack with Prometheus and Grafana',
+      'Built a PoC for an Internal Developer Portal (IDP) using Backstage',
+      'Led numerous optimization/refactoring projects for Terraform modules, simplifying the codebase, and cutting repeated code by over 50%'
     ]
   },
   {
@@ -21,7 +22,7 @@ const experiences = [
     highlights: [
       'Led DevOps tranformation during a period of rapid growth by automating many previously manual processes',
       'Designed and built CI/CD pipelines with Jenkins, cutting deployment times by 70%',
-      'Implmeneted SSO with Azure and reduced onboarding times from hours to minutes and saved $1500/mo by identifying wasteful user lisences'
+      'Implemented SSO with Azure and reduced onboarding times by several hours and saved $1500/mo by identifying wasteful user licenses'
     ]
   },
   {
@@ -30,8 +31,8 @@ const experiences = [
     period: '2018 - 2020',
     highlights: [
       'Maintained Windows servers and performed software/security updates on VMWare VMs',
-      'Cut monthly software spending costs by $2000/mo by migrating organization Jira and consolidating work management tools',
-      'Collaborated with development teams to improve deployment processes',
+      'Cut monthly software spending costs by $2000 by migrating the organization to Jira and consolidating work management tools',
+      'Coordinated with dev teams for performing bi-weekly roll-outs to production'
     ]
   }
 ]
